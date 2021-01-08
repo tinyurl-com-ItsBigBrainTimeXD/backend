426aca334335f6db503bc3a53ee7d5b9b44ec292
Wen Junhua <wenjh1998@gmail.com>
2021-01-08 15:47:57 +0800

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -9,10 +9,6 @@ from handler.iotHandler import iot_handler
 # Create the flask application
 app = Flask(__name__)
 
-# Create the database
-db = Database('test.db')
-
-
 # Create a basic route for debugging
 @app.route('/')
 def index():
@@ -27,11 +23,14 @@ def index():
 def frontend():
     """The endpoint for the frontend application to interact with"""
 
+    print(request.get_json())
     # Get the body and the request type
-    req_body = json.loads(request.get_json())
+    req_body = request.get_json()
     req_type = request.method
-
-    return frontend_handler(req_body, req_type, db)
+    db = Database('test.db')
+    result = frontend_handler(req_body, req_type, db)
+    del db
+    return json.dumps(result)
 
 
 # REST for IoT
@@ -43,7 +42,10 @@ def iot():
     req_body = json.loads(request.get_json())
     req_type = request.method
 
-    return iot_handler(req_body, req_type, db)
+    db = Database('test.db')
+    result = iot_handler(req_body, req_type, db)
+    del db
+    return result
     
 
 if __name__ == "__main__":
