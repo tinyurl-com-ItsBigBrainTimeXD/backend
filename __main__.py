56680340cd732ceb2b0d1de72172d62d171baff0
Wen Junhua <wenjh1998@gmail.com>
2021-01-08 15:08:57 +0800

    Update Frontend method

--- __main__.py
+++ __main__.py
@@ -1,11 +1,16 @@
+import json
 from flask import Flask, request
 from constants import HOST, PORT
+from Database.database import Database
 from handler.frontendHandler import frontend_handler
-from handler.iotHandler import iotHandler
+from handler.iotHandler import iot_handler
 
 
 # Create the flask application
 app = Flask(__name__)
+
+# Create the database
+db = Database('test.db')
 
 
 # Create a basic route for debugging
@@ -16,10 +21,18 @@ def index():
     """
     return '<h1>Hello world</h1>'
 
+
 # REST for frontend
 @app.route('/frontend', methods=['POST', 'GET', 'PUT', 'DELETE'])
 def frontend():
     """The endpoint for the frontend application to interact with"""
+
+    # Get the body and the request type
+    req_body = json.loads(request.get_json()) # Get as dict
+    req_type = request.method
+
+    return frontend_handler(req_body, req_type, db)
+
 
     # Get the body and the request type
     req_body = request.get_json()
