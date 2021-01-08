4fbf8de26d20c545c00fcbc237cb6463998d0986
Wen Junhua <wenjh1998@gmail.com>
2021-01-08 15:14:39 +0800

    Create get_helper.py

---
+++ handler/frontend_helper/get_helper.py
@@ -0,0 +1,30 @@
+from Database.database import Database
+from Core.helpers import generate_number, is_blank
+
+
+def search_handle(keyword: str, db: Database):
+    """Handle the searching of data"""
+    if is_blank(keyword):
+        raise KeyError('There is no keyword')
+    return db.search(keyword)
+
+
+def all_handle(keyword: str, db: Database):
+    """Return all the values in the database
+        Keyword is ignored.
+    """
+    return db.get_all()
+
+
+def get_random_id_handle(keyword: str, db: Database):
+    """Get a random ID
+        keyword and database is ignored.
+    """
+    return generate_number()
+
+
+get_dict = {
+    1: all_handle,
+    2: search_handle,
+    3: get_random_id_handle,
+}
