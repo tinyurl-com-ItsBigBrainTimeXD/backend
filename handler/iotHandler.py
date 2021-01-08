6123311cf03468f558ed645602d7fb568ba67a7a
Wen Junhua <wenjh1998@gmail.com>
2021-01-08 17:32:20 +0800

    Update iotHandler.py

--- handler/iotHandler.py
+++ handler/iotHandler.py
@@ -3,4 +3,4 @@ from Database.database import Database
 
 def iot_handler(req_body: dict, req_type: str, db: Database):
     """Handler for communication between the IoT devices"""
-    pass
+    return None
